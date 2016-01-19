8a9ecd26474d0dbb64decdd1c3a488428245f15c
rahulsahay19 <rahulsahay19@gmail.com>
2016-01-19 22:02:01 +0530

    2nd Commit

---
+++ CustomHTMLHelpers/CustomHelpers/HTMLButtonExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomHTMLHelpers.CustomHelpers
+{
+    public static class HTMLButtonExtension
+    {
+        public static MvcHtmlString BootstrapButton(
+            this HtmlHelper htmlHelper,
+            string innerHTML)
+        {
+            TagBuilder tb = new TagBuilder("button");
+
+            //Adding CSS class in the reverse order as last one will attach first
+            tb.AddCssClass("btn-primary");
+            tb.AddCssClass("btn");
+
+            tb.InnerHtml = innerHTML;
+
+            //Button Type is of submit
+            tb.MergeAttribute("type","submit");
+
+            return MvcHtmlString.Create(tb.ToString());
+        }
+    }
+}
---
+++ CustomHTMLHelpers/CustomHelpers/HTMLImageExtension.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomHTMLHelpers.CustomHelpers
+{
+    public static class HTMLImageExtension
+    {
+        //Using String Builder
+        public static MvcHtmlString ImageSb(this HtmlHelper htmlHelper,
+            string src, string altText)
+        {
+            StringBuilder sb = new StringBuilder(512);
+
+            sb.AppendFormat("<img src='{0}' alt='{1}' />", src, altText);
+
+            //return HTML encoded string
+            return MvcHtmlString.Create(sb.ToString());
+        }
+
+        //Using Tag Builder
+
+        public static MvcHtmlString Image(this HtmlHelper htmlHelper,
+            string src, 
+            string altText, 
+            string cssClass, 
+            string name,
+            object htmlAttrib =null)
+        {
+            TagBuilder tb = new TagBuilder("img");
+            tb.MergeAttribute("src", src);
+            tb.MergeAttribute("alt", altText);
+
+            //Just a null check, just to avoid any space or blank 
+            if (!string.IsNullOrWhiteSpace(cssClass))
+            {
+                tb.AddCssClass(cssClass);
+            }
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                name = TagBuilder.CreateSanitizedId(name);
+                //Then generate id using tagbuilder variable
+                tb.GenerateId(name);
+                tb.MergeAttribute("name",name);
+            }
+
+            tb.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttrib));
+            //return HTML encoded string
+            return MvcHtmlString.Create(tb.ToString(TagRenderMode.SelfClosing));
+
+        }
+
+        //Overloaded versions
+
+        public static MvcHtmlString Image(
+            this HtmlHelper htmlHelper,
+            string src,
+            string altText,
+            object htmlAttrib = null)
+        {
+            return HTMLImageExtension.Image(htmlHelper, src, altText, String.Empty, String.Empty, htmlAttrib);
+        }
+
+        public static MvcHtmlString Image(
+            this HtmlHelper htmlHelper,
+            string src,
+            string altText,
+            string cssClass,
+            object htmlAttrib = null)
+        {
+            return HTMLImageExtension.Image(htmlHelper, src, altText, cssClass, String.Empty, htmlAttrib);
+        }
+    }
+}
